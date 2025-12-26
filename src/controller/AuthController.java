7c5f1832131cbf33752eadf378500f96ac67f3bc
HELIOS <HELIOS@192.168.1.72>
2025-12-26 14:39:21 +0545

    1st

--- src/controller/AuthController.java
+++ src/controller/AuthController.java
@@ -1,19 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package controller;
-
-/**
- *
- * @author HELIOS
- */
-
-public class AuthController {
-    private final String adminUser = "admin";
-    private final String adminPass = "admin123";
-
-    public boolean login(String username, String password) {
-        return adminUser.equals(username) && adminPass.equals(password);
-    }
-}
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package controller;
+//
+///**
+// *
+// * @author HELIOS
+// */
+//
+//public class AuthController {
+//    private final String adminUser = "admin";
+//    private final String adminPass = "admin123";
+//
+//    public boolean login(String username, String password) {
+//        return adminUser.equals(username) && adminPass.equals(password);
+//    }
+//}
